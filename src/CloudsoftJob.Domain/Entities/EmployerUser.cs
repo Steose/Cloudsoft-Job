262445493e5565cc82b4f5b10281c087bc519391
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2026-04-20 16:27:32 +0200

    src/CloudsoftJob.Domain/

---
+++ src/CloudsoftJob.Domain/Entities/EmployerUser.cs
@@ -0,0 +1,10 @@
+namespace CloudsoftJob.Core.Models;
+
+public class EmployerUser
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string Email { get; set; } = string.Empty;
+
+    public string DisplayName { get; set; } = string.Empty;
+}
